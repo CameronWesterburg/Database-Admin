47ac5d633ef434659a188dba36dd193e9e52a78d
CameronWesterburg <cameronwesterburg@my.unt.edu>
2024-11-01 15:18:58 -0500

    Add files via upload

---
+++ Assignment 4/insert_game.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $output = 'Initial value';  
+            include 'include/connect.php'; // Connect to the database
+
+            if (! $dbconn ) {
+                echo 'Connection failed '.pg_last_error();
+            } else {
+                $player1_id = $_POST['player1_ID'];
+                $player2_id = $_POST['player1_ID'];
+                $player_command = "CALL rps.proc_insert_generic($1, $2, NULL)";
+
+                $result = pg_query_params($dbconn, $player_command, array($player1_id, $player2_id));
+
+            if (!$result) {
+                echo "Error in procedure call: " . pg_last_error();
+            } else {
+                $errlvl = pg_fetch_row($result)[0];
+
+                if($errlvl == '0') {
+                    $outputMessage = 'Success';
+                } else
+                    if($errlvl == '1') {
+                    $outputMessage = 'Invalid parameters';
+                } else
+                    if($errlvl == '2') {
+                    $outputMessage = 'Foreign Key Violation';
+                } else
+                    if($errlvl == '3') {
+                    $outputMessage = 'Unique game violation';
+                } else {
+                    $outputMessage = "unrecognized return code";
+                }
+            }
+            pg_close( $dbconn );
+            }
+        ?> // HTML boiler plate 
+         <form action="/index.html">
+            <div>
+                <textarea class="message" name="feedback" text-align: center;
+                        id="feedback" rows=1 cols=80 readonly="enabled">
+                    <?php echo $output; ?>
+                </textarea>
+            </div>
+                <button type="submit">Return</button>
+        </form>
+    </body>
+</html>
---
+++ Assignment 4/insert_player.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+           $output = 'Initial value';  
+           include 'include/connect.php'; // Connect to the database
+
+           if (! $dbconn ) {
+               echo 'Connection failed '.pg_last_error();
+            } else {
+               $player_id = $_POST['player_id'];
+               $player_command = "CALL rps.proc_insert_generic($1, NULL)";
+               
+               $result = pg_query_params($dbconn, $player_command, array($player_id));
+
+            if (!$result) {
+                echo "Error in procedure call: " . pg_last_error();
+            } else {
+
+                $errlvl = pg_fetch_row($result)[0];
+                if($errlvl == '0') {
+                    $outputMessage = 'Success';
+                } else
+                    if($errlvl == '1') {
+                    $outputMessage = 'Invalid parameters';
+                } else
+                    if($errlvl == '2') {
+                    $outputMessage = 'Foreign Key Violation';
+                } else {
+                    $outputMessage = "Unrecognized return code";
+                }
+            }
+            pg_close($dbconn); // Close the connection
+            echo $outputMessage;
+        }    
+        ?> // HTML boiler plate
+     <form action="/index.html">
+            <div>
+                <textarea class="message" name="feedback" text-align: center;
+                        id="feedback" rows=1 cols=80 readonly="enabled">
+                    <?php echo $outputMessage; ?>
+                </textarea>
+            </div>
+                <button type="submit">Return</button>
+        </form>
+    </body>
+</html>
